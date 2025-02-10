882a58f8c1c57c8963b33b0fdd2d378fac2d1569
KutayKoray <kutaykoray58@gmail.com>
2025-02-10 22:28:27 +0300

    Refactor: main put inside of a loop to imitate shell

--- main.c
+++ main.c
@@ -1,24 +1,50 @@
 #include "minishell.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PROMPT "minishell$ "
 
 int main(void) {
-    char *input = "ls -l | grep txt | wc -l >> output.txt";
-    t_token *tokens = lexer(input);
-    print_tokens(tokens);
+    char *input = NULL;
+    size_t len = 0;
+    
+    while (1) 
+    {
+        printf(PROMPT);
+        fflush(stdout);
 
-    expand_variables(tokens);
+        ssize_t read = getline(&input, &len, stdin);
+        if (read == -1) {
+            printf("\nÇıkılıyor...\n");
+            break;
+        }
 
-    printf("\nSonrası:\n");
-    print_tokens(tokens);
+        if (input[read - 1] == '\n') {
+            input[read - 1] = '\0';
+        }
 
-    printf("\nAST:\n");
-    t_ast *root = parse_tokens(tokens);
-    print_ast(root, 0);
+        if (strcmp(input, "exit") == 0) {
+            printf("Çıkılıyor...\n");
+            break;
+        }
 
-    printf("\nRunning AST...\n");
-    run_ast(root);
+        t_token *tokens = lexer(input);
+        if (!tokens) continue;
 
+        //print_tokens(tokens); for debugging
+
+        expand_variables(tokens);
+
+        t_ast *root = parse_tokens(tokens);
+        if (!root) continue;
+
+        // print_ast(root, 0); for debugging
+
+        run_ast(root);
+    }
+
+    free(input);
     return 0;
 }
-
-
