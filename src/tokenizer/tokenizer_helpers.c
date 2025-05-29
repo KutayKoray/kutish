d8eb3d7f6e74ed8c609bbddf11a24aeb0b3b97fb
Kutay Koray <kkoray@k1m11s03.42kocaeli.com.tr>
2025-05-29 20:12:41 +0300

    Added: Parser

---
+++ src/tokenizer/tokenizer_helpers.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer_helpers.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkoray <kkoray@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 12:33:08 by kkoray            #+#    #+#             */
+/*   Updated: 2025/04/26 12:33:09 by kkoray           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	operator_length(const char *s)
+{
+	if ((s[0] == '>' && s[1] == '>') || (s[0] == '<' && s[1] == '<'))
+		return (2);
+	return (1);
+}
+
+int	skip_space(const char *input, int i, int *prev_was_space)
+{
+	if (input[i] == ' ')
+	{
+		*prev_was_space = 1;
+		return (i + 1);
+	}
+	return (i);
+}
+
+int	is_operator_char(char c)
+{
+	return (c == '>' || c == '<' || c == '|');
+}
+
+int	read_quoted(const char *str, int start, char quote)
+{
+	int	i;
+
+	i = start + 1;
+	while (str[i] && str[i] != quote)
+		i++;
+	if (str[i] == quote)
+		i++;
+	return (i);
+}
+
+int	read_plain(const char *str, int start)
+{
+	int	i;
+
+	i = start;
+	while (str[i] && str[i] != '\'' && str[i] != '"' && str[i] != ' '
+		&& !is_operator_char(str[i]))
+		i++;
+	return (i);
+}
