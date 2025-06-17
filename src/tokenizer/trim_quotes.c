71cd372a7f17e756eb8da7f71606d831c1fbad50
lareii <lareii@proton.me>
2025-06-17 20:23:55 +0300

    fix(tokenizer): clean up quote processing logic and remove unused function

--- src/tokenizer/trim_quotes.c
+++ src/tokenizer/trim_quotes.c
@@ -5,37 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ebabaogl <ebabaogl@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/06 18:35:51 by ebabaogl          #+#    #+#             */
-/*   Updated: 2025/06/14 13:01:40 by ebabaogl         ###   ########.fr       */
+/*   Created: 2025/06/17 20:20:14 by ebabaogl          #+#    #+#             */
+/*   Updated: 2025/06/17 20:20:16 by ebabaogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "libft.h"
 #include <stdlib.h>
-
-static char	*remove_inner_matching_quotes(const char *str, char quote)
-{
-	char	*result;
-	int		i;
-	int		j;
-	size_t	len;
-
-	i = 0;
-	j = 0;
-	len = ft_strlen(str);
-	result = malloc(len + 1);
-	if (!result)
-		return (NULL);
-	while (str[i])
-	{
-		if (str[i] != quote)
-			result[j++] = str[i];
-		i++;
-	}
-	result[j] = '\0';
-	return (result);
-}
 
 static void	skip_and_copy_quoted(const char *value, int *i,
 	char *cleaned, int *j)
@@ -75,26 +52,11 @@ static char	*remove_mixed_quotes(const char *value)
 
 static void	process_token_quotes(t_token *token)
 {
-	char	*trimmed;
 	char	*cleaned;
-	size_t	len;
-	char	quote;
 
-	len = ft_strlen(token->value);
-	if (len >= 2
-		&& ((token->value[0] == '"' && token->value[len - 1] == '"')
-			|| (token->value[0] == '\'' && token->value[len - 1] == '\'')))
-	{
-		quote = token->value[0];
-		token->trimmed = 1;
-		trimmed = ft_substr(token->value, 1, len - 2);
-		if (!trimmed)
-			return ;
-		cleaned = remove_inner_matching_quotes(trimmed, quote);
-		free(trimmed);
-	}
-	else
-		cleaned = remove_mixed_quotes(token->value);
+	cleaned = remove_mixed_quotes(token->value);
+	if (!cleaned)
+		return ;
 	free(token->value);
 	token->value = cleaned;
 }
